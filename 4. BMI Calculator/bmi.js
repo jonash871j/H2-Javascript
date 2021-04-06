8d90b5a7785a3ab51c6d108d1d5e28480029877d
Jonas hofmann <Jonash871j@gmail.com>
2021-04-06 15:03:05 +0200

    Commit

--- 4. BMI Calculator/bmi.js
+++ 4. BMI Calculator/bmi.js
@@ -4,7 +4,48 @@ function validateForm()
     var height = document.forms["bmiForm"]["tb_height"].value;
     var bmi = weight / (height ** 2)
 
+    var bmiInfo = "";
+    var bmiColor = "";
+
+    if (bmi < 16)
+    {
+        bmiInfo = "invalid";
+        bmiColor = "black"
+    }
+    else if (bmi <= 18.5)
+    {
+        bmiInfo = "body weight deflict";
+        bmiColor = "DodgerBlue"
+    }
+    else if (bmi <= 24)
+    {
+        bmiInfo = "norm";
+        bmiColor = "Aquamarine"
+    }
+    else if (bmi <= 30)
+    {
+        bmiInfo = "weight over";
+        bmiColor = "ForestGreen"
+    }
+    else if (bmi <= 35)
+    {
+        bmiInfo = "obesity first degree";
+        bmiColor = "Yellow"
+    }
+    else if (bmi <= 40)
+    {
+        bmiInfo = "obesity second degree";
+        bmiColor = "Orange"
+    }
+    else
+    {
+        bmiInfo = "obesity thrid degree";
+        bmiColor = "Red"
+    }
+
     document.getElementById("bmi").innerHTML = bmi;
+    document.getElementById("bmiInfo").innerHTML = bmiInfo;
+    document.getElementById("bmiInfo").style.color = bmiColor;
 
     return false;
 }
