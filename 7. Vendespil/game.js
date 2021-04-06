c349d07ea484204a04ccdc282afa6449c16596c1
Jonas hofmann <Jonash871j@gmail.com>
2021-04-06 13:38:49 +0200

    Commit

--- 7. Vendespil/game.js
+++ 7. Vendespil/game.js
@@ -1,39 +1,93 @@
-class GameManager{
+class Card{
+    constructor(value)
+    {
+        this.value = value;
+        this.isSelected = false;
+        this.isFound = false;
+    }
+}
+
+class Board
+{
     constructor(cardAmount)
     {
+        this.cardAmount = cardAmount;
+
         if (cardAmount % 2 != 0)
         {
             console.error("uneven card amount " + cardAmount);
         }
 
-        this.cardAmount = cardAmount;
-        this.board = [];
+        this.cards = [];
+        this.reset();
+    }
 
-        for (var i = 0; i < cardAmount; i++)
-        {
-            this.board[i] = -1;
+    reset(){
+        for (var i = 0; i < this.cardAmount; i++){
+            this.cards[i] = new Card(-1);
         }
 
-        for (var i = 0; i < cardAmount; i += 2)
-        {
+        for (var i = 0; i < this.cardAmount; i += 2){
             var randomNumber = Math.floor(Math.random() * 1000);
 
-            this.board[i] = randomNumber;
-            this.board[i + 1] = randomNumber;
+            this.placeCardRandonAvailableCell(randomNumber);
+            this.placeCardRandonAvailableCell(randomNumber);
+        }
+    }
+    placeCardRandonAvailableCell(value){
+        while(true){
+            var index = Math.floor(Math.random() * this.cardAmount);
+            if (this.cards[index].value == -1){
+                this.cards[index].value = value;
+                return;
+            }
         }
     }
 }
 
+class GameManager{
+    constructor(cardAmount){
+        this.cardAmount = cardAmount;
+        this.board = new Board(cardAmount);
+    }
+
+}
+
 var game;
+var cellElements;
 
 function initGame(cardAmount) {
     game = new GameManager(cardAmount)
-    
+
+    updateGame();
+
+    var cardIndex = 0;
+    cellElements = document.querySelectorAll('[data-cell]');
+    cellElements.forEach(cell => {
+        cell.addEventListener('click', handleClick, {once: false});
+        cell.classList.add(cardIndex);
+        cardIndex++;
+    })
+}
+
+function handleClick(e) {
+    console.log(e.target.classList);
+}
+
+function updateGame() {
     var boardElement = document.getElementById("boardId");
 
-    for (var i = 0; i < game.cardAmount; i++)
-    {
-        boardElement.innerHTML += "<div class=\"cell\">" +  this.game.board[i] + "</div>";
-    }
+    for (var i = 0; i < game.cardAmount; i++){
+        var card = game.board.cards[i];
+        var cardText = "";
 
+        if (card.isFound || card.isSelected){
+            cardText = card.value;
+        }
+        else{
+            cardText = "?";
+        }
+
+        boardElement.innerHTML += "<div class=\"cell\" data-cell>" + cardText + "</div>";
+    }
 }
